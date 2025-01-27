8ed22299d3529391a473928d9f9f6f406a412c67
Aric S. Jones <aric.jones@student.msdmartinsville.org>
2025-01-27 10:20:45 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/your-repo-name',
+  basePath: '/https://github.com/skj0nes/NextJsGhPagesV6',
 };
 
 export default nextConfig;
